c769a8690a4be114d42eea9694d1ac9764b0ae96
Omar Latreche <latreche.omar@gmail.com>
2015-10-14 22:02:16 +0200

    Création de la première solution
    Première solution de type console pour tester
    le fonctionnement d'une entité intélligente.

---
+++ TX52_A15_sln1/TX52_A15_sln1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Projet réalisé dans le cadre de l'unité d'enseignement TX52 à 
+ * l'Univertisté Technologique de Belfort-Montbéliard
+ * par Thomas Jubé et Omar Latreche
+ * Encadré par Alain-Jerome Fougeres
+ */
+
+
+namespace TX52_A15_sln1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
